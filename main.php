321c1c47b6e0702e566d480645ee7a3c96fd0bd9
Tim Hsu <tim37021@gmail.com>
2014-01-11 03:39:02 +0800

    Rewrite die() as stop()
    stop() outputs error message into STDERR as usual applications.

--- main.php
+++ main.php
@@ -3,9 +3,9 @@ define("STRING_FRET", 8);
 define("STRING_PRESS", 6);
 define("STRING_RELEASE", 7);
 
- count($argv) == 3 or die("Usage: php -e $argv[0] <config_script> <score_script>\n");
+ count($argv) == 3 or stop("Usage: php -e $argv[0] <config_script> <score_script>\n");
  $latency = read_config($argv[1]);
- $sheet = file_get_contents($argv[2]) or die("Error reading $argv[2]\n");
+ $sheet = file_get_contents($argv[2]) or stop("Error reading $argv[2]\n");
  preg_match_all("/\^?\(((\d\d)+)\)(\d+)/", $sheet, $score, PREG_SET_ORDER);
  $string_map = array();
  $tempo = array("tempo" => 60, "divisions" => 1, "unit" => null);
@@ -17,7 +17,7 @@ define("STRING_RELEASE", 7);
    if (!array_key_exists($string_id, $string_map))
     $string_map[$string_id] = array();
    if (array_key_exists($time_axis, $string_map[$string_id]))
-    die("Conflict operation on the same string.\n");
+    stop("Conflict operation on the same string.\n");
    $string_map[$string_id][$time_axis] = array("position" => $position, "duration" => (int)$tuple[3]);
   }
   $time_axis += $tuple[3];
@@ -33,7 +33,7 @@ define("STRING_RELEASE", 7);
    foreach ($action as $command) {
     $real_time = $time * $tempo["unit"] + $command["time"];
     if ($last_attack !== null && $real_time < $last_attack + $latency["fret"])
-     die("No enough time for playing.\n");
+     stop("No enough time for playing.\n");
     if (!array_key_exists($real_time, $command_map))
      $command_map[$real_time] = array();
     array_push($command_map[$real_time], $command["code"]);
@@ -58,7 +58,7 @@ class guitar_string
 
  public function guitar_string($id)
  {
-  0 < $id && $id <= 16 or die("Invalid ID for guitar_string class.\n");
+  0 < $id && $id <= 16 or stop("Invalid ID for guitar_string class.\n");
   $this->id = $id - 1;
  }
 
@@ -119,36 +119,36 @@ class guitar_string
 function read_config($source)
 {
  $latency = array();
- $config = file_get_contents($source) or die("Error reading $source\n");
+ $config = file_get_contents($source) or stop("Error reading $source\n");
  $config = preg_split("/\n|\r/", $config, -1, PREG_SPLIT_NO_EMPTY);
  foreach ($config as $line) {
   $data = preg_split("/\s/", $line, -1, PREG_SPLIT_NO_EMPTY);
   switch ($data[0]) {
    case "fret_latency":
-    count($data) == 2 or die("Error fret_latency assignment\n");
-    !array_key_exists("fret", $latency) or die("Conflict assignment of fret_latency\n");
+    count($data) == 2 or stop("Error fret_latency assignment\n");
+    !array_key_exists("fret", $latency) or stop("Conflict assignment of fret_latency\n");
     $latency["fret"] = to_positive_int($data[1], "Error format of fret_latency\n");
    break;
    case "move_latency":
-    count($data) == 4 or die("Error move_latency assignment\n");
+    count($data) == 4 or stop("Error move_latency assignment\n");
     if (!array_key_exists("move", $latency))
      $latency["move"] = array();
     $data[1] = to_positive_int($data[1], "Error format of move_latency parameter #1\n");
     $data[2] = to_positive_int($data[2], "Error format of move_latency parameter #2\n");
     if (!array_key_exists($data[1], $latency["move"]))
      $latency["move"][$data[1]] = array();
-    !array_key_exists($data[2], $latency["move"][$data[1]]) or die("Conflict assignment of move_latency $data[1] $data[2]\n");
-    $data[1] != $data[2] or die("move_latency cannot apply to the same position\n");
+    !array_key_exists($data[2], $latency["move"][$data[1]]) or stop("Conflict assignment of move_latency $data[1] $data[2]\n");
+    $data[1] != $data[2] or stop("move_latency cannot apply to the same position\n");
     $latency["move"][$data[1]][$data[2]] = to_positive_int($data[3], "Error format of move_latency parameter #3\n");
    break;
    case "press_latency":
-    count($data) == 2 or die("Error press_latency assignment\n");
-    !array_key_exists("press", $latency) or die("Conflict assignment of press_latency\n");
+    count($data) == 2 or stop("Error press_latency assignment\n");
+    !array_key_exists("press", $latency) or stop("Conflict assignment of press_latency\n");
     $latency["press"] = to_positive_int($data[1], "Error format of press_latency\n");
    break;
    case "release_latency":
-    count($data) == 2 or die("Error release_latency assignment\n");
-    !array_key_exists("release", $latency) or die("Conflict assignment of release_latency\n");
+    count($data) == 2 or stop("Error release_latency assignment\n");
+    !array_key_exists("release", $latency) or stop("Conflict assignment of release_latency\n");
     $latency["release"] = to_positive_int($data[1], "Error format of release_latency\n");
    break;
   }
@@ -164,9 +164,15 @@ function read_config($source)
  return $latency;
 }
 
+function stop($msg)
+{
+ fprintf(STDERR, $msg);
+ exit(1);
+}
+
 function to_positive_int($n, $e)
 {
- preg_match("/^\s*[1-9]\d*\s*$/", $n) or die($e);
+ preg_match("/^\s*[1-9]\d*\s*$/", $n) or stop($e);
  return (int)$n;
 }
 ?>
